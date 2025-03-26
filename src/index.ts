b0bc610ab447a9c403a5f0894088e5711e260072
anish877 <62011810+anish877@users.noreply.github.com>
2025-03-26 22:11:23 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "*",
+  origin: "https://draw-next-frontend.vercel.app",
   credentials: true
 }));
 
