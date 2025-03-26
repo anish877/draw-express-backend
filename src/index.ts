b2aacb713814c29f7cdf52e6c48c4f439ecbc724
anish877 <62011810+anish877@users.noreply.github.com>
2025-03-26 13:07:33 +0530

    push

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import { prismaClient } from "./db";
